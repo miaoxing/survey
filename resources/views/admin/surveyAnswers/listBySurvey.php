eb148062708505e3a8bdf7c70923b514e76cd694
twinh <twinhuang@qq.com>
2019-02-23 12:47:18 +0800

    refactoring: 移除bootprompt，直接更新bootbox到5.0

--- resources/views/admin/surveyAnswers/listBySurvey.php
+++ resources/views/admin/surveyAnswers/listBySurvey.php
@@ -48,7 +48,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['plugins/admin/js/data-table', 'form', 'plugins/app/js/bootbox/bootbox', 'plugins/admin/js/date-range-picker'], function () {
+  require(['plugins/admin/js/data-table', 'form', 'plugins/app/js/bootbox', 'plugins/admin/js/date-range-picker'], function () {
     var recordTable = $('.js-survey-answer-table').dataTable({
       ajax: {
         url: $.url('admin/survey-answers/list-by-survey.json', {surveyId: <?= $this->e($req['surveyId']); ?>})
